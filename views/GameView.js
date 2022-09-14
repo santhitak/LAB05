4e334ba6f6af3d24b74f134c1c4cab08f4737140
santhitak <santhita.sk@gmail.com>
2022-09-15 02:06:53 +0700

    refactor: gameview

--- views/GameView.js
+++ views/GameView.js
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
-import {
-  View,
-  StyleSheet,
-  Button,
-  TextInput,
-  ImageBackground,
-} from "react-native";
+import { View, StyleSheet, Button, TextInput } from "react-native";
 import { Text } from "@ui-kitten/components";
 import Colors from "../constants/colors";
 
 const GameView = ({ correctNumber, onOver }) => {
-  const [guess, setGuess] = useState("");
-
   const [round, setRound] = useState(0);
   const [guessNum, setGuessNum] = useState(0);
   const [confirmed, setConfirmed] = useState(false);
@@ -19,27 +11,23 @@ const GameView = ({ correctNumber, onOver }) => {
 
   let confirmedOutput;
 
-  const check = (val) => {
-    if (correctNumber > val) {
-      setGuess("Too Low");
-    } else if (correctNumber < val) {
-      setGuess("Too High");
-    } else if (correctNumber === val) {
-      setGuess("Correct");
-    }
-  };
-
   if (confirmed) {
     confirmedOutput = (
       <View style={styles.resultContainer}>
-        <Text style={{ color: "white" }}>You selected</Text>
+        <Text style={styles.pills} category="h3">
+          {correctNumber < guessNum
+            ? "Too High"
+            : correctNumber > guessNum
+            ? "Too Low"
+            : "Correct"}
+        </Text>
         <View style={styles.numberContainer}>
           <Text style={styles.number}>{guessNum}</Text>
         </View>
-        <Text style={{ color: "white" }} category="h3">
-          {guess}
+        ;
+        <Text style={styles.pills} category="h5">
+          {round} rounds guessed.
         </Text>
-        ;<Text style={{ color: "white" }}>{round} rounds guessed.</Text>
       </View>
     );
   }
@@ -55,53 +43,43 @@ const GameView = ({ correctNumber, onOver }) => {
   const confirmInputHandler = () => {
     let val = parseInt(enteredValue);
     setGuessNum(val);
-    check(val);
     setRound(round + 1);
-    setGuessNum(val);
-    if (val === correctNumber) onOver(round || 1);
+    if (val === correctNumber) onOver(round);
     setConfirmed(true);
   };
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={{
-          uri: "https://i.pinimg.com/736x/4f/da/a5/4fdaa5eb789e923a53635c3007660b2b--bit-art-tokyo-japan.jpg",
-        }}
-        resizeMode="cover"
-        style={styles.wrapping}
-      >
-        <View style={styles.card}>
-          <Text category="h4">Guess a Number</Text>
-          <TextInput
-            style={styles.input}
-            blurOnSubmit
-            autoCapitalize="none"
-            autoCorrect={false}
-            keyboardType="number-pad"
-            maxLength={2}
-            value={enteredValue}
-            onChange={numberInputHandler}
-          />
-          <View style={styles.buttonContainer}>
-            <View>
-              <Button
-                title="Reset"
-                color={Colors.accent}
-                onPress={resetInputHandler}
-              />
-            </View>
-            <View>
-              <Button
-                title="Confirm"
-                color={Colors.primary}
-                onPress={confirmInputHandler}
-              />
-            </View>
+      <View style={styles.card}>
+        <Text category="h4">Guess</Text>
+        <TextInput
+          value={enteredValue}
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+          onChangeText={numberInputHandler}
+        />
+        <View style={styles.buttonContainer}>
+          <View>
+            <Button
+              title="Reset"
+              color={Colors.accent}
+              onPress={resetInputHandler}
+            />
+          </View>
+          <View>
+            <Button
+              title="Confirm"
+              color={Colors.primary}
+              onPress={confirmInputHandler}
+            />
           </View>
         </View>
-        {confirmedOutput}
-      </ImageBackground>
+      </View>
+      {confirmedOutput}
     </View>
   );
 };
@@ -110,10 +88,6 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: "100%",
-  },
-  wrapping: {
-    width: "100%",
-    height: "101%",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
@@ -148,23 +122,30 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   resultContainer: {
-    marginTop: 20,
     alignItems: "center",
     height: 30,
     marginVertical: 10,
+    marginBottom: 300,
   },
   numberContainer: {
-    borderWidth: 2,
-    borderColor: Colors.accent,
-    padding: 10,
+    border: `5px solid ${Colors.textdark}`,
+    backgroundColor: Colors.textlight,
+    paddingHorizontal: 35,
+    paddingVertical: 5,
     borderRadius: 10,
     marginVertical: 10,
     alignItems: "center",
     justifyContent: "center",
   },
   number: {
-    color: Colors.accent,
-    fontSize: 22,
+    color: Colors.textdark,
+    fontSize: 32,
+  },
+  pills: {
+    backgroundColor: "rgba(5, 8, 8, .55)",
+    color: Colors.textlight,
+    paddingHorizontal: 20,
+    borderRadius: 30,
   },
 });
 
