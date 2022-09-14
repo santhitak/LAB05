a8ff11030fab4a19e302fc383ad32fa0acaf6d0b
santhitak <santhita.sk@gmail.com>
2022-09-15 00:33:47 +0700

    feat: rnd

--- views/StartGameView.js
+++ views/StartGameView.js
@@ -3,10 +3,14 @@ import React from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import Colors from "../constants/colors";
 
-const StartGameView = (props) => {
+const StartGameView = ({ onStartGame }) => {
+  const ran = () => {
+    Math.floor(Math.random() * 100);
+    console.log(Math.floor(Math.random() * 100));
+  };
   return (
     <View style={styles.View}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => onStartGame(ran)}>
         <Text category="h2" style={styles.glow}>
           START
         </Text>
