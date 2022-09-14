264bcb24eca9167186a0d1d230a8915a11439500
santhitak <santhita.sk@gmail.com>
2022-09-15 02:07:45 +0700

    bugs: fixed dumpest little bug

--- views/StartGameView.js
+++ views/StartGameView.js
@@ -4,10 +4,8 @@ import { View, StyleSheet, TouchableOpacity } from "react-native";
 import Colors from "../constants/colors";
 
 const StartGameView = ({ onStartGame }) => {
-  let ran = () => {
-    Math.floor(Math.random() * 100);
-    console.log(Math.floor(Math.random() * 100));
-  };
+  let ran = Math.floor(Math.random() * 100);
+  console.log(ran);
   return (
     <View style={styles.View}>
       <TouchableOpacity
